167841a297a0aa927fe30733e36019047ec17c45
Mateus Augusto dos Santos Fonseca <mateus.asfonseca@gmail.com>
2022-09-30 17:30:27 -0300

    Testes corrigidos

--- AdaCalculatorTeste/AdaCalculatorUnitTests.cs
+++ AdaCalculatorTeste/AdaCalculatorUnitTests.cs
@@ -14,53 +14,121 @@ namespace AdaCalculatorTeste
             _sut = new CalculatorMachine(_mock.Object);
         }
 
-        #region Testes com Mock
+        #region Teste Com Mock e Com valor
+
+        [Theory]
+        [InlineData("multiply", 100, 2, 200)]
+        [InlineData("multiply", 100, -2, -200)]
+        [InlineData("multiply", 100, -3, -300)]
+        [InlineData("multiply", 100, 0, 0)]
+        public void CalcultatorMachine_MultiplicacaoDeNumeros_CalculoEChamadaCorreta(string op, double num1, double num2, double resultado)
+        {
+            CalculatorMachine cf = new CalculatorMachine();
+            var lala = cf.Calculate(op, num1, num2);
+
+            _sut.Calculate(op, num1, num2);
+
+            _mock.Setup(x => x.Calculate(op, num1, num2)).Returns((lala));
+
+            Assert.Equal(op, lala.operation);
+            Assert.Equal(resultado, lala.result);
+
+            _mock.Verify(x => x.Calculate(op, num1, num2), Times.Once);
+        }
+
+
         [Theory]
         [InlineData("sum", 100, 2, 102)]
         [InlineData("sum", 100, -2, 98)]
         [InlineData("sum", 100, 0, 100)]
-        public void CalculatorMachine_SomaDeNumerosInteiros_ResultadoValidoComMock(string op, double num1, double num2, double resultado)
+        public void CalcultatorMachine_SomaDeNumeros_CalculoEChamadaCorreta(string op, double num1, double num2, double resultado)
         {
-            _mock.Setup(x => x.Calculate(op, num1, num2)).Returns((op, resultado));
+            CalculatorMachine cf = new CalculatorMachine();
+            var lala = cf.Calculate(op, num1, num2);
+
             _sut.Calculate(op, num1, num2);
+
+            _mock.Setup(x => x.Calculate(op, num1, num2)).Returns((lala));
+
+            Assert.Equal(op, lala.operation);
+            Assert.Equal(resultado, lala.result);
+
             _mock.Verify(x => x.Calculate(op, num1, num2), Times.Once);
         }
-
 
         [Theory]
         [InlineData("subtract", 100, 40, 60)]
         [InlineData("subtract", 100, -2, 102)]
         [InlineData("subtract", 100, 0, 100)]
-        public void CalculatorMachine_SubtracaoDeNumerosInteiros_ResultadoValidoComMock(string op, double num1, double num2, double resultado)
+        public void CalcultatorMachine_SubtracaoDeNumeros_CalculoEChamadaCorreta(string op, double num1, double num2, double resultado)
         {
-            _mock.Setup(x => x.Calculate(op, num1, num2)).Returns((op, resultado));
+            CalculatorMachine cf = new CalculatorMachine();
+            var lala = cf.Calculate(op, num1, num2);
+
             _sut.Calculate(op, num1, num2);
+
+            _mock.Setup(x => x.Calculate(op, num1, num2)).Returns((lala));
+
+            Assert.Equal(op, lala.operation);
+            Assert.Equal(resultado, lala.result);
+
             _mock.Verify(x => x.Calculate(op, num1, num2), Times.Once);
         }
-
-
-        [Theory]
-        [InlineData("multiply", 100, 2, 200)]
-        [InlineData("multiply", 100, -2, 200)]
-        [InlineData("multiply", 100, -3, -300)]
-        [InlineData("multiply", 100, 0, 0)]
-        public void CalculatorMachine_MultiplicacaoDeNumerosInteiros_ResultadoValidoComMock(string op, double num1, double num2, double resultado)
-        {
-            _mock.Setup(x => x.Calculate(op, num1, num2)).Returns((op, resultado));
-            _sut.Calculate(op, num1, num2);
-            _mock.Verify(x => x.Calculate(op, num1, num2), Times.Once);
-        }
-
 
         [Theory]
         [InlineData("divide", 100, 2, 50)]
         [InlineData("divide", 100, -2, -50)]
         [InlineData("divide", 100, 0, double.PositiveInfinity)]
-        public void CalculatorMachine_DivisaoDeNumeros_ResultadoValidoComMock(string op, double num1, double num2, double resultado)
+        public void CalcultatorMachine_DivisaoDeNumeros_CalculoEChamadaCorreta(string op, double num1, double num2, double resultado)
         {
-            _mock.Setup(x => x.Calculate(op, num1, num2)).Returns((op, resultado));
+            CalculatorMachine cf = new CalculatorMachine();
+            var lala = cf.Calculate(op, num1, num2);
+
             _sut.Calculate(op, num1, num2);
+
+            _mock.Setup(x => x.Calculate(op, num1, num2)).Returns((lala));
+
+            Assert.Equal(op, lala.operation);
+            Assert.Equal(resultado, lala.result);
+
             _mock.Verify(x => x.Calculate(op, num1, num2), Times.Once);
+        }
+        #endregion
+
+        #region Testes com Mock
+        [Fact]
+        public void CalculatorMachine_SomaDeNumeros_ChamadaCorretaComMock()
+        {
+            _mock.Setup(x => x.Calculate("sum", 2, 2)).Returns(("sum", 4));
+            _sut.Calculate("sum", 2, 2);
+            _mock.Verify(x => x.Calculate("sum", 2, 2), Times.Once);
+        }
+
+
+        [Fact]
+        public void CalculatorMachine_SubtracaoDeNumeros_ChamadaCorretaComMock()
+        {
+            _mock.Setup(x => x.Calculate("subtract", 2, 2)).Returns(("subtract", 0));
+            _sut.Calculate("subtract", 2, 2);
+            _mock.Verify(x => x.Calculate("subtract", 2, 2), Times.Once);
+        }
+
+
+        [Fact]
+        public void CalculatorMachine_MultiplicacaoDeNumeros_ChamadaCorretaComMock()
+        {
+            _mock.Setup(x => x.Calculate("multiply", 2, 2)).Returns(("multiply", 4));
+            _sut.Calculate("multiply", 2, 2);
+            _mock.Verify(x => x.Calculate("multiply", 2, 2), Times.Once);
+        }
+
+
+        [Fact]
+        public void CalculatorMachine_DivisaoDeNumeros_ChamadaCorretaComMock()
+        {
+            _mock.Setup(x => x.Calculate("divide", 2, 2)).Returns(("divide", 1));
+            _sut.Calculate("divide", 2, 2);
+            _mock.Verify(x => x.Calculate("divide", 2, 2), Times.Once);
         }
         #endregion
 
